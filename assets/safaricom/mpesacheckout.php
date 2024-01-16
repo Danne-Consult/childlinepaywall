c66d9fba7c247ac0a8cd9ebbc0a826cf086e840b
Danne Consult <danneconsult@gmail.com>
2024-01-16 18:51:09 +0300

    Checkout v1

--- assets/safaricom/mpesacheckout.php
+++ assets/safaricom/mpesacheckout.php
@@ -6,11 +6,11 @@ date_default_timezone_set("Africa/Nairobi");
 /*Call function with these configurations*/
 $consumer_key = '3c3QceNnq2bXIm3vy466MdAxnAbR6cQP';
 $consumer_secret = 'DObr8G3ZuWjeAB6A';
-$Business_Code = '1160116';
+$Business_Code = '174379';
 $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
 $Type_of_Transaction = 'CustomerPayBillOnline';
 $Token_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-$OnlinePayment = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$OnlinePayment = 'https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest';
 $CallBackURL = 'https://danneconsult.com/client/childlinepaywall/mpesapayreceiver.php';
 $Time_Stamp = date("Ymdhis");
 $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
@@ -53,7 +53,7 @@ $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
             'Password' => $password,
             'Timestamp' =>$Time_Stamp,
             'TransactionType' =>$Type_of_Transaction,
-            'Amount' => $itemamount,
+            'Amount' => $donoramount,
             'PartyA' => $donorphone,
             'PartyB' => $Business_Code,
             'PhoneNumber' => $donorphone,
