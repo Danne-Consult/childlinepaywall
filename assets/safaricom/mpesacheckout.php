a585d87783449dd86e25c0685e6cacf26f73bfb0
Danne Consult <danneconsult@gmail.com>
2024-01-16 21:59:35 +0300

    mpesa checkout error removed

--- assets/safaricom/mpesacheckout.php
+++ assets/safaricom/mpesacheckout.php
@@ -10,8 +10,8 @@ $Business_Code = '174379';
 $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
 $Type_of_Transaction = 'CustomerPayBillOnline';
 $Token_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-$OnlinePayment = 'https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest';
-$CallBackURL = 'https://danneconsult.com/client/childlinepaywall/mpesapayreceiver.php';
+$OnlinePayment = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$CallBackURL = 'https://danneconsult.com/client/childlinepaywall/assets/safaricom/mpesapayreceiver.php';
 $Time_Stamp = date("Ymdhis");
 $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
 
@@ -43,14 +43,15 @@ $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
         $curl_Tranfer_response = curl_exec($curl_Tranfer);
 
         $token = json_decode($curl_Tranfer_response)->access_token;
+        
 
         $curl_Tranfer2 = curl_init();
         curl_setopt($curl_Tranfer2, CURLOPT_URL, $OnlinePayment);
-        curl_setopt($curl_Tranfer2, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $token));
+        curl_setopt($curl_Tranfer2, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer '.$token));
 
         $curl_Tranfer2_post_data = [
             'BusinessShortCode' => $Business_Code,
-            'SecurityCredential' => $password,
+            'Password' => $password,
             'Timestamp' =>$Time_Stamp,
             'TransactionType' =>$Type_of_Transaction,
             'Amount' => $donoramount,
@@ -63,7 +64,6 @@ $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
         ];
 
         $data2_string = json_encode($curl_Tranfer2_post_data);
-
         curl_setopt($curl_Tranfer2, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl_Tranfer2, CURLOPT_POST, true);
         curl_setopt($curl_Tranfer2, CURLOPT_POSTFIELDS, $data2_string);
@@ -71,14 +71,20 @@ $password = base64_encode($Business_Code . $Passkey . $Time_Stamp);
         curl_setopt($curl_Tranfer2, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($curl_Tranfer2, CURLOPT_SSL_VERIFYHOST, 0);
         $curl_Tranfer2_response = json_decode(curl_exec($curl_Tranfer2));
-
+        
+        curl_close($curl_Tranfer2);
+        
+        var_dump($curl_Tranfer2_response);
+        
+        
         $result = $curl_Tranfer2_response->ResponseCode; 
         $requestid = $curl_Tranfer2_response->CheckoutRequestID;
         
         if($result === "0"){
-            echo $result ."/n" . $requestid; 
-
+          echo $result ."/n" . $requestid;
+          
         }
         
+
     }
 
